32b423dc62d6067d9cbc4f6fc7880c0988fbbfeb
Ivanova Taya <emperortaika@gmail.com>
2022-10-25 23:58:28 +0300

    add proc file

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const rateLimit = require('express-rate-limit');
 
-const { DBURL = 'mongodb://localhost:27017/db' } = process.env;
+const { DBURL = 'mongodb://0.0.0.0:27017/db' } = process.env;
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
